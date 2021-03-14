f743f96b0f58a445e7a6d1fe9df628c12563a0bb
nph <carllegrone@gmail.com>
2021-03-14 17:01:13 -0400

    well this is painful

---
+++ src/Component.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { highlight, languages } from "prismjs";
+
+  export let borderColor = "black";
+  export let code = "<em>test</em>";
+  $: highlighted = highlight(code, languages.html, "html");
+
+  const onInput = (e) => {
+    code = e.target.innerText;
+  };
+</script>
+
+<div class="wrap" style="--border-color: {borderColor}">
+  <pre
+    on:input={onInput}
+    contenteditable
+    class="language-html"><code class="language-html">{@html highlighted}
+    </code></pre>
+  <div class="output">
+    {@html code}
+  </div>
+</div>
+
+<style>
+  .wrap {
+    display: flex;
+    border: var(--border-color) 2px solid;
+  }
+
+  pre {
+    height: 5em;
+    margin: 0;
+    flex-grow: 1;
+  }
+
+  .output {
+    margin: 8px;
+    flex-grow: 1;
+  }
+</style>
---
+++ src/dev.js
@@ -0,0 +1,8 @@
+import Component from "./Component.svelte";
+import "prismjs-tomorrow-theme/prism-tomorrow.css";
+
+const app = new Component({
+  target: document.getElementById("svelte"),
+});
+
+export default app;
